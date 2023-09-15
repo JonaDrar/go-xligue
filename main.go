a6dfcadedb0f65fed5de8641428eb7e32f4c76bd
Jonathan Araya <j.arayarobles@gmail.com>
2023-09-14 23:09:34 -0300

    [feat]: connect to Mongo

--- main.go
+++ main.go
@@ -1,14 +1,63 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	uri := os.Getenv("MONGODB_URI")
+
+	if uri == "" {
+		log.Fatal("You must set the MONGODB_URI environment variable")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	coll := client.Database("sample_mflix").Collection("movies")
+
+	title := "Back to the Future"
+
+	var result bson.M
+	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Println("No document found")
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+
+	jsonData, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", jsonData)
 
 	port := os.Getenv("PORT")
 
