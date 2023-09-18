a8613d20c406a0bbe62dca3c2f9b77279749912b
Jonathan Araya <j.arayarobles@gmail.com>
2023-09-18 13:32:53 -0300

    [fix]: restore -v

--- configs/env.go
+++ configs/env.go
@@ -1,6 +1,7 @@
 package configs
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -8,6 +9,10 @@ import (
 )
 
 func EnvMongoURI() string {
+
+	//saber todos los archivos que tengo en el path
+	fmt.Println(os.Environ())
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
