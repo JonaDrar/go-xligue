1266274c780c1dce88eabcd8be41614a5df56306
Jonathan Araya <j.arayarobles@gmail.com>
2023-09-18 01:48:42 -0300

    [fix]: restore -v

--- configs/env.go
+++ configs/env.go
@@ -3,12 +3,13 @@ package configs
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func EnvMongoURI() string {
-	err := godotenv.Load()
+	err := godotenv.Load(filepath.Join("", ".env"))
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
