d237974ef147924d341ea1a6ac21a33d07987899
Jonathan Araya <j.arayarobles@gmail.com>
2023-09-18 13:50:15 -0300

    [fix]: restore -v

--- configs/setup.go
+++ configs/setup.go
@@ -12,13 +12,13 @@ import (
 
 func ConnectDB() *mongo.Client {
 	opts := options.Client().ApplyURI(EnvMongoURI())
-	opts.SetConnectTimeout(2 * time.Second)
+	opts.SetConnectTimeout(10 * time.Second)
 	client, err := mongo.Connect(context.Background(), opts)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	err = client.Ping(ctx, nil)
