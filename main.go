08a2b39d4c08d2f73ad100aea93f748801bf44f4
Jonathan Araya <j.arayarobles@gmail.com>
2023-09-18 01:21:50 -0300

    [fix] go -v for fl0

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/jonadrar/go-xligue/configs"
 	"github.com/jonadrar/go-xligue/routes"
@@ -12,8 +14,14 @@ func main() {
 	//run DB connection
 	configs.ConnectDB()
 
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
 	//routes
 	routes.UserRoute(app)
 
-	app.Listen(":3000")
+	app.Listen(":" + port)
 }
